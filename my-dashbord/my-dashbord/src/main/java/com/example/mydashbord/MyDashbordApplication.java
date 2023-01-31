eea9398304eb9e7dcbef021fdb1297394fecf98d
RidaBadr <114596641+RidaBadr@users.noreply.github.com>
2023-01-31 11:42:37 +0100

    second commit

---
+++ my-dashbord/my-dashbord/src/main/java/com/example/mydashbord/MyDashbordApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDashbordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDashbordApplication.class, args);
+	}
+
+}
---
+++ my-dashbord/my-dashbord/src/test/java/com/example/mydashbord/MyDashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
